53f42b6cefbd25db1c0f8def3a903c11f720c577
Kurt McGallion <kurt.mcgallion@gmail.com>
2017-02-09 16:27:27 -0600

    Almost Done

--- js/index.js
+++ js/index.js
@@ -8,14 +8,57 @@ $(document).ready(function(){
 
 
   });
-  $(".fifth img").hover(function() {
-
-    $(".fifth2 img").fadeTo(400, .5);
-    $(".fifth2-overlay").css("display", "block");
+  $(".fifth2").hover(function() {
+    $(".fadeimg2").fadeTo(400, 0.5);
+    $(".fifth2-overlay").css("display", "flex");
 }, function() {
-
-    $(".fifth2 img").fadeTo(400,1);
+    $(".fadeimg2").fadeTo(400,1);
     $(".fifth2-overlay").css("display", "none");
+  });
+
+
+  $(".fifth3").hover(function() {
+    $(".fadeimg3").fadeTo(400, 0.5);
+    $(".fifth3-overlay").css("display", "flex");
+}, function() {
+    $(".fadeimg3").fadeTo(400,1);
+    $(".fifth3-overlay").css("display", "none");
+  });
+
+
+  $(".fifth4").hover(function() {
+    $(".fadeimg4").fadeTo(400, 0.5);
+    $(".fifth4-overlay").css("display", "flex");
+}, function() {
+    $(".fadeimg4").fadeTo(400,1);
+    $(".fifth4-overlay").css("display", "none");
+  });
+
+
+  $(".fifth5").hover(function() {
+    $(".fadeimg5").fadeTo(400, 0.5);
+    $(".fifth5-overlay").css("display", "flex");
+}, function() {
+    $(".fadeimg5").fadeTo(400,1);
+    $(".fifth5-overlay").css("display", "none");
+  });
+
+
+  $(".fifth6").hover(function() {
+    $(".fadeimg6").fadeTo(400, 0.5);
+    $(".fifth6-overlay").css("display", "flex");
+}, function() {
+    $(".fadeimg6").fadeTo(400,1);
+    $(".fifth6-overlay").css("display", "none");
+  });
+
+
+  $(".fifth7").hover(function() {
+    $(".fadeimg7").fadeTo(400, 0.5);
+    $(".fifth7-overlay").css("display", "flex");
+}, function() {
+    $(".fadeimg7").fadeTo(400,1);
+    $(".fifth7-overlay").css("display", "none");
   });
 
 });
