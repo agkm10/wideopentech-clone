964680bc575341bfcb222a99e657e60b5abf3c5c
Kurt McGallion <kurt.mcgallion@gmail.com>
2017-02-08 23:28:24 -0600

    saving for the day

--- js/index.js
+++ js/index.js
@@ -8,5 +8,14 @@ $(document).ready(function(){
 
 
   });
+  $(".fifth img").hover(function() {
+
+    $(".fifth2 img").fadeTo(400, .5);
+    $(".fifth2-overlay").css("display", "block");
+}, function() {
+
+    $(".fifth2 img").fadeTo(400,1);
+    $(".fifth2-overlay").css("display", "none");
+  });
 
 });
